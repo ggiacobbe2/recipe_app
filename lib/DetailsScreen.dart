611555502aa65c4c94aaf4d16d806f18ab7226de
Giada Giacobbe <ggiacobbe13@gmail.com>
2025-10-06 15:08:54 -0400

    building UI for DetailsScreen

--- lib/DetailsScreen.dart
+++ lib/DetailsScreen.dart
@@ -10,6 +10,28 @@ class DetailsScreen extends StatelessWidget {
     required this.title,
     required this.image,
     required this.description,
-
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(image),
+            const SizedBox(height: 16),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(description),
+          ],
+        ),
+      ),
+    );
+  }
 }
