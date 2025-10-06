779520d270adbdcf373c5095bf4d43768590d7d9
Giada Giacobbe <ggiacobbe13@gmail.com>
2025-10-06 15:03:28 -0400

    declaring title, image, and description vars & a constructor for details screen

--- lib/DetailsScreen.dart
+++ lib/DetailsScreen.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  
+
+  final String title;
+  final String image;
+  final String description;
+
+  const DetailsScreen({
+    required this.title,
+    required this.image,
+    required this.description,
+
   });
+}
